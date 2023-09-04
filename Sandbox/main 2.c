bf39762f561dd5ab1510d33b1cc64ba024df13d1
Evan Day <yichaoday_evan@berkeley.edu>
2023-09-04 15:42:16 -0700

    initial push

---
+++ Sandbox/main 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+
+
+int x2 (int n) { return 2*n; }
+int x10(int n) { return 10*n; }
+void mutate_map(int A[], int n, int(*fp)(int)) {
+    for (int i = 0; i < n; i++)
+        A[i] = (*fp)(A[i]);
+}
+void print_array(int A[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ",A[i]);
+    printf("\n");
+}
+
+int main(void) {
+    int A[] = {3,1,4};
+    int *p = A;
+    printf("A: %p, &A: %p\n", A, &A);
+    printf("p: %p, &p: %p\n", p, &p);
+
+}
+
